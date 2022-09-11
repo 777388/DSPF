fdb074b189092255f7d1bad2fa33c2eea72a6680
777388 <96343159+777388@users.noreply.github.com>
2022-09-11 09:23:46 -0700

    Update DSPF.py

--- DSPF.py
+++ DSPF.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 
-print("Usage: python3 DSPF.py path/to/iplist.txt output.txt path/to/fuzz.txt path/to/params.txt")
+print("Usage: python DSPF.py path/to/iplist.txt output.txt path/to/fuzz.txt path/to/params.txt")
 input1 = sys.argv[1]
 output1 = open(sys.argv[2], 'w')
 fuzz = sys.argv[3]
@@ -15,11 +15,11 @@ with open(input1, 'r') as filer:
                     for paramline in filed:
                     if (len(line.rstrip()) and len(paramline.rstrip()) and len(fuzzline.rstrip())):
                         process = os.popen("curl -m 3.3 -so /dev/null https://"+line.rstrip()+"/?"+paramline.rstrip()+"="+fuzzline.rstrip()+" -w %{http_code}").read()
-                        if process == '200' or process == '403':
+                        if process == '200':
                             print("https://"+line.rstrip()+"/?"+paramline.rstrip()+"="+fuzzline.rstrip()+"    "+process+"\r")
                             print("https://"+line.rstrip()+"/?"+paramline.rstrip()+"="+fuzzline.rstrip()+"    "+process+"\r", file=output1)
                         process = os.popen("curl -m 3.3 -so /dev/null http://"+line.rstrip()+"/?"+paramline.rstrip()+"="+fuzzline.rstrip()+" -w %{http_code}").read()
-                        if process == '200' or process == '403':
+                        if process == '200':
                             print("http://"+line.rstrip()+"/?"+paramline.rstrip()+"="+fuzzline.rstrip()+"     "+process+"\r")
                             print("http://"+line.rstrip()+"/?"+paramline.rstrip()+"="+fuzzline.rstrip()+"     "+process+"\r", file=output1)
                     else:
